d33f648f05d57beb9caa6ea5cf9d9c3254b1c03e
ramilbacay <ramilbacay@gmail.com>
2018-05-30 14:14:11 +0800

    Edited index, css, js
    Added tabs, designed tabs, added functions on click of tabs

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
+document.getElementById('employees').onclick = function(event)
+{
+    document.getElementById('teams').classList.remove("active");
+    document.getElementById('awards').classList.remove("active");
+    this.classList.add("active");
+    document.getElementById('h2').innerHTML = "Please select the employees you wish to tag.";
+ 	document.getElementById('h5').innerHTML = "Tip: You can filter the employees displayed using the filters on the side.";
+}
 
+document.getElementById('teams').onclick = function(event)
+{
+ 	document.getElementById('employees').classList.remove("active");
+ 	document.getElementById('awards').classList.remove("active");
+ 	this.classList.add("active");
+ 	this.classList.remove("disabled");
+ 	document.getElementById('h2').innerHTML = "Please select the team of selected employee/s.";
+ 	document.getElementById('h5').innerHTML = "Tip: You can select multiple teams.";
+}
+
+document.getElementById('awards').onclick = function(event)
+{
+    
+    document.getElementById('employees').classList.remove("active");
+    document.getElementById('teams').classList.remove("active");
+    this.classList.add("active");
+    document.getElementById('h2').innerHTML = "Please select the awards tag of selected employee/s.";
+ 	document.getElementById('h5').innerHTML = "Tip: You can select multiple tags.";
+}
