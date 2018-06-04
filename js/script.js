262fb9db1bce414444cea9ac7ec60a7ae261789d
ramilbacay <ramilbacay@gmail.com>
2018-06-05 00:16:24 +0800

    Added the filter
    Fixed the layout, added employee checkbox and buttons.

--- js/script.js
+++ js/script.js
@@ -7,17 +7,42 @@ document.getElementById('employees').onclick = function(event)
  	document.getElementById('h5').innerHTML = "Tip: You can filter the employees displayed using the filters on the side.";
 }
 
-document.getElementById('teams').onclick = function(event)
+document.getElementById('addTeam').onclick = function(event)
+{
+	document.getElementById('teams').classList.add("active");
+	document.getElementById('employees').classList.remove("active");
+	document.getElementById('teams').classList.remove("disabled");
+	document.getElementById('h2').innerHTML = "Please select the team of selected employee/s.";
+ 	document.getElementById('h5').innerHTML = "Tip: You can select multiple teams.";
+ 	document.getElementById('teamsContent').style.display = "block";
+ 	document.getElementById('employeesContent').style.display = "none";
+ 	document.getElementById('addTeam').style.display = "none";
+}
+
+function filterResult() {
+	if (document.getElementById('cle').checked) {
+	document.getElementById('steph').style.display = "none";
+	document.getElementById('durant').style.display = "none";
+	document.getElementById('klay').style.display = "none";
+	}
+	else
+	{
+		document.getElementById('steph').style.display = "block";
+		document.getElementById('durant').style.display = "block";
+		document.getElementById('klay').style.display = "block";
+	}
+}
+
+/*document.getElementById('teams').onclick = function(event)
 {
  	document.getElementById('employees').classList.remove("active");
  	document.getElementById('awards').classList.remove("active");
  	this.classList.add("active");
  	this.classList.remove("disabled");
- 	document.getElementById('h2').innerHTML = "Please select the team of selected employee/s.";
- 	document.getElementById('h5').innerHTML = "Tip: You can select multiple teams.";
-}
+ 	
+}*/
 
-document.getElementById('awards').onclick = function(event)
+/*document.getElementById('awards').onclick = function(event)
 {
     
     document.getElementById('employees').classList.remove("active");
@@ -25,4 +50,4 @@ document.getElementById('awards').onclick = function(event)
     this.classList.add("active");
     document.getElementById('h2').innerHTML = "Please select the awards tag of selected employee/s.";
  	document.getElementById('h5').innerHTML = "Tip: You can select multiple tags.";
-}
+}*/
